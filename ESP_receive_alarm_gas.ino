6c25af57e2dfce59f805741e671cd9ec5bfa3505
AnastasiaCotov <43670516+AnastasiaCotov@users.noreply.github.com>
2018-11-21 10:52:16 +0200

    Add files via upload

---
+++ ESP_receive_alarm_gas.ino
@@ -0,0 +1,116 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <SoftwareSerial.h>
+SoftwareSerial pitch(13, 15, false, 128);
+SoftwareSerial roll(12, 14, false, 128);
+
+int buzzer = 14;
+int fum = A0;
+int sensorThres = 400;
+uint32_t analog_Smoke_Sensor;
+
+const char* ssid = "no";
+const char* password = "14151415";
+
+
+int buf[50];
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+  pinMode(fum, INPUT);
+  
+  Serial.begin(115200);
+  pitch.begin(115200);
+  roll.begin(115200);
+  // We start by connecting to a WiFi network
+  WiFi.begin(ssid, password);
+  //Serial.print("Wait for WiFi... ");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("Connecting..");
+  }
+  delay(50);
+ /* Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());*/
+  
+  //server stuff
+  const uint16_t port = 443;
+  const char * host = "http://fathomless-citadel-40087.herokuapp.com"; // ip or dns
+  /*Serial.print("connecting to ");
+  Serial.println(host);*/
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    /*Serial.println("connection failed");
+    Serial.println("wait 5 sec...");*/
+    delay(5000);
+    return;
+  }
+  else{
+   // Serial.println("connected");
+  }
+}
+
+void loop() {
+  analog_Smoke_Sensor = analogRead(fum);
+  Serial.print("sens smoke/gas value: ");
+  Serial.println(analog_Smoke_Sensor);
+  if (analog_Smoke_Sensor > sensorThres + 300)
+  {
+    int j = 200; // The starting pitch
+    while(j < 500)
+    {
+      j++;
+      tone(buzzer, j); // Emit the noise
+      delay(5);
+    }
+  }
+  else
+  {
+    noTone(buzzer);
+  }
+    
+  HTTPClient http;
+  String postData = String("http://fathomless-citadel-40087.herokuapp.com/device/1") + String("?data=") + String(analog_Smoke_Sensor);
+  http.begin(postData);  
+  int httpCode = http.GET(); 
+  //Serial.println(httpCode);
+  int b = rx();
+  Serial.print("pitch = ");
+  Serial.println(b);
+  String postPitch = String("http://fathomless-citadel-40087.herokuapp.com/device/2") + String("?data=") + String(b);
+  http.begin(postPitch);
+  int httpCode1 = http.GET();
+  
+  int c = rx1();
+  Serial.print("roll = ");
+  Serial.println(c);
+  String postRoll = String("http://fathomless-citadel-40087.herokuapp.com/device/3") + String("?data=") + String(c);
+  http.begin(postRoll);
+  int httpCode2 = http.GET();
+  
+  if (httpCode > 0) { //Check the returning code
+  String payload = http.getString();   //Get the request response payload
+  //Serial.println(payload);                     //Print the response payload
+  }
+  http.end();   //Close connection
+  delay(3000);
+}
+int rx()
+{
+  if (pitch.available() > 0)
+  {
+    int p = pitch.read();
+    return p;
+  }
+}
+int rx1()
+{
+  if (roll.available() > 0)
+  {
+    int r = roll.read();
+    return r;
+  }
+}
---
+++ valAccelerometru_TXESP.ino
@@ -0,0 +1,49 @@
+#include <Wire.h>
+#include <MPU6050.h>
+
+#define ADXL_SCL 21
+#define ADXL_SDA 20
+
+#include <SoftwareSerial.h>
+SoftwareSerial hackPitch(0, 1);
+SoftwareSerial hackRoll(5, 6);
+SoftwareSerial hackFall(8, 9);
+MPU6050 mpu;
+
+int fall;
+
+void setup() 
+{
+  Serial.begin(115200);
+  hackPitch.begin(115200);
+  hackRoll.begin(115200);
+  hackFall.begin(115200);
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+}
+
+void loop()
+{
+  // Read normalized values 
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  // Calculate Pitch & Roll
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
+  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
+
+  // Output
+  Serial.print(" Pitch = ");
+  Serial.print(pitch);
+  Serial.print(" Roll = ");
+  Serial.print(roll);
+  Serial.println();
+  delay(10);
+  hackPitch.write(pitch);
+  hackRoll.write(roll);
+  hackFall.write(fall);
+}
+
+
